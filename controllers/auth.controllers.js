7467fe38ac223b186d22b0e7f4cd35fd9f18200c
SushilBhandary <sushil.bhandary.b@gmail.com>
2023-03-12 02:20:54 +0530

    adding auth controllers and middleware

---
+++ controllers/auth.controllers.js
@@ -0,0 +1,95 @@
+const User = require( '../models/user.schema.js')
+const asyncHandler = require( '../services/asyncHandler')
+const CustomError = require( '../utils/customError')
+const cookieOptions = require( '../Utils/cookieOptions')
+
+/******************************************************
+ * @SIGNUP
+ * @route /api/auth/signup
+ * @description User signUp Controller for creating new user
+ * @parameters name, email, password
+ * @returns User Object
+ ******************************************************/
+exports.signUp = asyncHandler(async (req, res) => {
+    const {name, email, password } = req.body
+
+    if (!name || !email || !password) {
+        throw new CustomError('Please fill all fields', 400)
+    }
+    //check if user exists
+    const existingUser = await User.findOne({email})
+
+    if (existingUser) {
+        throw new CustomError('User already exists', 400)  
+    }
+
+    const user = await User.create({
+        name,
+        email,
+        password
+    });
+    const token = user.getJwtToken()
+    console.log(user);
+    user.password = undefined
+
+    res.cookie("token", token, cookieOptions)
+
+    res.status(200).json({
+        success: true,
+        token,
+        user
+    })
+
+})
+
+/******************************************************
+ * @LOGIN
+ * @route /api/auth/login
+ * @description User signIn Controller for loging new user
+ * @parameters  email, password
+ * @returns User Object
+ ******************************************************/
+exports.login = asyncHandler(async (req, res) => {
+    const { email, password } = req.body
+
+    if ( !email || !password) {
+        throw new CustomError('Please fill all fields', 400)
+    }
+
+    const user = await User.findOne({email}).select("+password")
+
+    if (!user) {
+        throw new CustomError('Invalid credentials', 400)
+    }
+
+    const isPasswordMatched = await user.comparePassword(password)
+
+    if (isPasswordMatched) {
+        const token = user.getJwtToken()
+        user.password = undefined;
+        res.cookie("token", token, cookieOptions)
+        return res.status(200).json({
+            success: true,
+            token,
+            user
+        })
+    }
+    throw new CustomError('Invalid credentials - pass', 400)
+
+})
+
+
+/******************************************************
+ * @LOGOUT
+ * @route /api/auth/logout
+ * @description User logout by clearing user cookies
+ * @parameters  
+ * @returns success message
+ ******************************************************/
+exports.logout = asyncHandler(async (_req, res) => {
+    res.clearCookie()
+    res.status(200).json({
+        success: true,
+        message: "Logged Out"
+    })
+})
---
+++ middlewares/auth.middlewares.js
@@ -0,0 +1,20 @@
+const JWT = require("jsonwebtoken")
+const config = require("../config/index")
+const asyncHandler = require('../services/asyncHandler')
+const CustomError =  require('../utils/customError')
+const User = require( "../models/user.schema.js")
+
+exports.auth = asyncHandler( async(req, res, next) => {
+    const token = req.headers.authorization
+    if (!token) {
+        throw new CustomError('Not authorized to access this route', 401)
+    }
+    try {
+        const decodedJwtPayload = JWT.verify(token, config.JWT_SECRET)
+        req.user = await User.findById(decodedJwtPayload._id, "name email role")
+        req.user = decode
+        return next()
+    } catch (error) {
+        throw new CustomError('Not authorized to access this route', 401)
+    }
+})
