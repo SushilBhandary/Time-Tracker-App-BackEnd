5ddd97d8b72f79b069936715c1d802d2c9982b27
SushilBhandary <sushil.bhandary.b@gmail.com>
2023-03-14 20:33:05 +0530

    adding task controllers

--- controllers/task.controllers.js
+++ controllers/task.controllers.js
@@ -23,7 +23,7 @@ exports.createTask = asyncHandler( async(req, res) => {
         date : date,
         task : task._id
     })
-    for (let index = 1; index < 24; index++) {
+    for (let index = 0; index < 24; index++) {
         task.action.push({
             description : '', 
             startTime : index, 
@@ -79,4 +79,97 @@ exports.updateAction = asyncHandler( async(req, res) => {
         message: "Update action successfully",
         task
     })
+})
+
+/******************************************************
+ * @Update_Task
+ * @route /api/task/updatetask/:taskid
+ * @description To Update task
+ * @parameters why , feel
+ * @returns task Object
+ ******************************************************/
+exports.updatetask = asyncHandler( async(req, res) => {
+    const { why , feel} = req.body
+    const { taskid } = rea.param
+    let task = await Task.findById(taskid)
+    const { Urgent, Important, urgentVsImportantMatrix } = calculateMatrix(task)
+    const totalHouseRecoded = calculateTotalHouse(task)
+    task = await Task.findByIdAndUpdate( taskid, {Urgent, Important, urgentVsImportantMatrix, why , feel, totalHouseRecoded} )
+    return res.status(200).json({
+        success: true,
+        message: "Updated task successfully",
+        task
+    })
+})
+
+/******************************************************
+ * @Calculate_Matrix_of_Urgent_and_Important 
+ * @description To calculate urgent and important matrix
+ * @parameters task
+ * @returns Object
+ ******************************************************/
+const calculateMatrix = asyncHandler( (task) => {
+    //Urgent VS Important 
+    let yesYes  = 0, noNo = 0, yesNo = 0, noYes = 0, 
+    urgentYes = 0, urgentNo = 0, ImportantYes = 0, ImportantNo = 0
+    task.action.forEach(element => {
+        if (element.Urgent === true && element.Important === true) {
+            yesYes += 1
+            urgentYes += 1
+            ImportantYes += 1
+        } else if (element.Urgent === false && element.Important === false) {
+            noNo += 1
+            urgentNo += 1
+            ImportantNo += 1
+        } else if (element.Urgent === true && element.Important === false) {
+            yesNo += 1
+            urgentYes += 1
+            ImportantNo += 1
+        } else if (element.Urgent === false && element.Important === true) {
+            noYes += 1
+            urgentNo += 1
+            ImportantYes += 1
+        }
+    });
+    yesYes = (Math.round(yesYes * 100) / 24).toFixed(2);
+    noNo = (Math.round(yesYes * 100) / 24).toFixed(2);
+    yesNo = (Math.round(yesYes * 100) / 24).toFixed(2);
+    noYes = (Math.round(yesYes * 100) / 24).toFixed(2);
+    urgentYes = (Math.round(urgentYes * 100) / 24).toFixed(2);
+    ImportantYes = (Math.round(ImportantYes * 100) / 24).toFixed(2);
+    urgentNo = (Math.round(urgentNo * 100) / 24).toFixed(2);
+    ImportantNo = (Math.round(ImportantNo * 100) / 24).toFixed(2);
+    return {
+        Urgent : {
+            yes : urgentYes ,
+            no : urgentNo 
+        }, 
+        Important : {
+            yes : ImportantYes ,
+            no : ImportantNo 
+        },
+        urgentVsImportantMatrix : {
+            yesYes,
+            noNo,
+            yesNo,
+            noYes
+        }
+    }
+    
+})
+
+/******************************************************
+ * @Calculate_Total_House
+ * @description To calculate Total house
+ * @parameters task
+ * @returns totalHouseRecoded
+ ******************************************************/
+const calculateTotalHouse = asyncHandler( (task) => {
+    let count
+    task.action.forEach(element => {
+        if (!element.description) {
+            count +=1
+        }
+    });
+    return count
 })
