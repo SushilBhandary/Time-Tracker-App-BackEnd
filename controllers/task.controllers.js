9bd5a6581317fea9026efed9aecc9d846e96b275
SushilBhandary <sushil.bhandary.b@gmail.com>
2023-04-08 23:41:03 +0530

    change on createTask, updateAction, updatetask

--- controllers/task.controllers.js
+++ controllers/task.controllers.js
@@ -12,13 +12,23 @@ const CustomError = require( '../utils/customError')
  ******************************************************/
 exports.createTask = asyncHandler( async(req, res) => {
     const {date} = req.body
-    const tracker = await Tracker.findById(req.usre.tracker)
-    tracker.dayTraker.forEach(element => {
-        if (element.date===date) {
-            throw new CustomError('Date already exists', 400)
+    const tracker = await Tracker.findById(req.user.tracker)
+    let element
+    for (let index = 0; index < tracker.dayTraker.length; index++) {
+        element = tracker.dayTraker[index]
+        if ( new Date(element.date).getDate() === new Date(date).getDate() && 
+        new Date(element.date).getFullYear() === new Date(date).getFullYear() &&
+        new Date(element.date).getMonth() === new Date(date).getMonth()) {
+            const task = await Task.findById(element.task)
+            return res.status(200).json({
+                success: true,
+                message: "Getting task",
+                task,
+                date : `${new Date(date).getDate()}/${new Date(date).getMonth()}/${new Date(date).getFullYear()}`
+            })
         }
-    });
-    const task = await Task.create()
+    }
+    const task = await Task.create({})
     tracker.dayTraker.push( {
         date : date,
         task : task._id
@@ -37,7 +47,8 @@ exports.createTask = asyncHandler( async(req, res) => {
     return res.status(200).json({
         success: true,
         message: "Task created successfully",
-        task
+        task,
+        date : `${new Date(date).getDate()}/${new Date(date).getMonth()}/${new Date(date).getFullYear()}`
     })
 })
 
@@ -69,10 +80,10 @@ exports.addAction = asyncHandler( async(req, res) => {
  * @returns task Object
  ******************************************************/
 exports.updateAction = asyncHandler( async(req, res) => {
-    const { description, startTime, endTime, Urgent, Important } = req.body
-    const { taskid } = req.param
+    const { action } = req.body
+    const { taskid } = req.params
     const task = await Task.findById(taskid)
-    task.action = task.action.map( t => t.startTime===startTime ? {description, startTime, endTime, Urgent, Important} : t)
+    task.action = action
     await task.save()
     return res.status(200).json({
         success: true,
@@ -90,10 +101,62 @@ exports.updateAction = asyncHandler( async(req, res) => {
  ******************************************************/
 exports.updatetask = asyncHandler( async(req, res) => {
     const { why , feel} = req.body
-    const { taskid } = rea.param
+    const { taskid } = req.params
     let task = await Task.findById(taskid)
-    const { Urgent, Important, urgentVsImportantMatrix } = calculateMatrix(task)
-    const totalHouseRecoded = calculateTotalHouse(task)
+    //-----------------------------------------
+    //Urgent VS Important 
+    let yesYes  = 0, noNo = 0, yesNo = 0, noYes = 0, 
+    urgentYes = 0, urgentNo = 0, ImportantYes = 0, ImportantNo = 0
+    task.action.forEach(element => {
+        if (element.Urgent === true && element.Important === true) {
+            yesYes += 1
+            urgentYes += 1
+            ImportantYes += 1
+        } else if (element.Urgent === false && element.Important === false) {
+            noNo += 1
+            urgentNo += 1
+            ImportantNo += 1
+        } else if (element.Urgent === true && element.Important === false) {
+            yesNo += 1
+            urgentYes += 1
+            ImportantNo += 1
+        } else if (element.Urgent === false && element.Important === true) {
+            noYes += 1
+            urgentNo += 1
+            ImportantYes += 1
+        }
+    });
+    yesYes = (Math.round(yesYes * 100) / 24).toFixed(2);
+    noNo = (Math.round(noNo * 100) / 24).toFixed(2);
+    yesNo = (Math.round(yesNo * 100) / 24).toFixed(2);
+    noYes = (Math.round(noYes * 100) / 24).toFixed(2);
+    urgentYes = (Math.round(urgentYes * 100) / 24).toFixed(2);
+    ImportantYes = (Math.round(ImportantYes * 100) / 24).toFixed(2);
+    urgentNo = (Math.round(urgentNo * 100) / 24).toFixed(2);
+    ImportantNo = (Math.round(ImportantNo * 100) / 24).toFixed(2);
+    const { Urgent, Important, urgentVsImportantMatrix } =  {
+        Urgent : {
+            yes : urgentYes ,
+            no : urgentNo 
+        }, 
+        Important : {
+            yes : ImportantYes ,
+            no : ImportantNo 
+        },
+        urgentVsImportantMatrix : {
+            yesYes,
+            noNo,
+            yesNo,
+            noYes
+        }
+    }
+    // calculate Total House
+    let totalHouseRecoded = 0
+    task.action.forEach(element => {
+        if (element.description) {
+            totalHouseRecoded +=1
+        }
+    });
     task = await Task.findByIdAndUpdate( taskid, {Urgent, Important, urgentVsImportantMatrix, why , feel, totalHouseRecoded} )
     return res.status(200).json({
         success: true,
