3252d4a7ee45d2b6c73e6401787c9559ddb82c32
SushilBhandary <sushil.bhandary.b@gmail.com>
2023-04-08 23:41:52 +0530

    updating routes

--- routes/Routes.js
+++ routes/Routes.js
@@ -6,6 +6,12 @@ const  {
     login,
     logout
 } = require('../controllers/auth.controllers')
+const {
+    createTask,
+    addAction,
+    updateAction,
+    updatetask
+} =require('../controllers/task.controllers')
 
 router.get("/", (req, res) => {
     res.send("Hello auth system")
@@ -13,5 +19,9 @@ router.get("/", (req, res) => {
 router.post("/api/auth/signup",signUp);
 router.post("/api/auth/login", login);
 router.post("/api/auth/logout", auth, logout);
+router.post("/api/createTask", auth, createTask);
+router.post("/api/addAction", auth, addAction);
+router.put("/api/updateAction/:taskid", auth, updateAction);
+router.put("/api/updatetask/:taskid", auth, updatetask);
 
 module.exports = router;
